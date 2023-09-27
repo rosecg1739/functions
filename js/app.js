6a1de29cb51e026f52c3a1174b22e23b1c1272cd
rosecg1739 <chelseagallagher05@gmail.com>
2023-09-27 18:10:10 -0400

    problem 4

--- js/app.js
+++ js/app.js
@@ -32,6 +32,16 @@ console.log(isCharAVowel(v));
 
 //problem 4 
 
+function sumArray(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    } 
+    return sum;
+}
+
+console.log(sumArray([5,4,2]));
+
 
 
         
