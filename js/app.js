a337e7074152f14186dd65b31689548fcacbf42d
rosecg1739 <chelseagallagher05@gmail.com>
2023-09-27 15:32:25 -0400

    learning how functions work

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
 console.log("it works !!");
+
+// defining a function
+// declaring a function
+// creationg reusibale block of code
+// to be used in a later time in the porogram
+
+function sayHello() {
+
+console.log("say hello");
+}
+
+sayHello();
+ 
+// write the function name ()
+// runs the block of code
+
+function sayGoodbye(personsName) {
+    console.log(`goodbye ${personsName}`)
+}
+
+console.log(`adios, Laura`);
+console.log(`chao. Becky`);
+console.log(`Goodbye, Chad`);
+console.log(`peace, Frankie`);
+
